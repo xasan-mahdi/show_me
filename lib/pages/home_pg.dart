99aa26c385afd9a7c4dc5f8edaec4ff016cc28e5
zuldaan xarago <zuldaanxarago@gmail.com>
2021-12-25 05:50:39 -0800

    day16 completed

--- lib/pages/home_pg.dart
+++ lib/pages/home_pg.dart
@@ -43,12 +43,36 @@ class _home_pgState extends State<home_pg> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? ListView.builder(
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,mainAxisSpacing: 20,crossAxisSpacing: 11),
           itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) =>
-             itemwidget(
-              item: CatalogModel.items[index],
-            ),
+          itemBuilder: (context, index) {
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)),
+              child: GridTile(
+                header: Container(
+                  child: Text(item.name,style: 
+                TextStyle(color: Colors.black,fontWeight: FontWeight.bold,),textAlign: TextAlign.center),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xFFffcccc),Color(0xFF59FFFF),Color(0xFF00FF00),
+                  ],begin: Alignment.topRight,end: Alignment.topLeft)
+                ),),
+                child: Image.network(item.image,fit: BoxFit.cover,),
+                footer: Container(child: Text("\$${item.price}",textScaleFactor: 1.2,
+                textAlign: TextAlign.right,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xFFFD0505),Color(0xFF06080F),
+                  ],begin: Alignment.topRight,end: Alignment.topLeft)
+                ),),));
+          }
         )
         : Center(child: CircularProgressIndicator(),),
       ),
